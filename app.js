512a53daa634e46c07c30f6a113cddc0b34ef1e1
Isaac <ikehunter5@gmail.com>
2018-09-23 13:56:19 -0400

    added node dependencies and routing on app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const express         = require('express');
+const app             = express();
+const bodyParser      = require('body-parser');
+const Controller      = require('./controllers/controller.js');
+const hbs             = require('hbs');
+const fs              = require('fs');
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+const port = process.env.PORT;
+
+Controller(app);
+
+app.listen(port, () => {
+  console.log(`Starting on port ${port}`);
+});
